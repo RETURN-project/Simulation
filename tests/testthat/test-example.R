bf8a1f091ac41b31b8e365f2f009f3d031835f05
wandadk <wandadekeersmaecker@gmail.com>
2020-02-13 14:37:35 +0100

    added test for the decomposition function, corrected documentation

--- tests/testthat/test-example.R
+++ tests/testthat/test-example.R
@@ -1,7 +1,47 @@
-context("Description of the battery of tests")
+context("Characterise time series")
 
-test_that("Name of this test", {
+test_that("Decomposition harmonic + noise", {
 
-  expect_equal(1, 1, tolerance = 1e-4)
+  source_rmd <- function(file, local = FALSE, ...){
+    options(knitr.duplicate.label = 'allow')
+
+    tempR <- tempfile(tmpdir = ".", fileext = ".R")
+    on.exit(unlink(tempR))
+    knitr::purl(file, output=tempR, quiet = TRUE)
+
+    envir <- globalenv()
+    source(tempR, local = envir, ...)
+  }
+
+  source_rmd('fun_simulate.Rmd')
+
+  set.seed(197)
+  # generate time stamps
+  tm <- seq(as.Date('2000-01-01'),as.Date('2005-12-31'), by='1 month',)
+
+  # seasonality represented by harmonic function
+  seas <- 0.5*sin(2*pi*(1:(12*6))/12)
+
+  # trend
+  tr <- 0.7
+
+  # noise
+  rn <- runif(12*6, min=0, max=100)/200
+  rn <- rn - mean(rn)
+
+  # time series equals sum of components
+  tsi <- as.data.frame(t(c(1,2,t(seas + tr + rn))))
+  names(tsi) <- c('lat','lon', tm)
+
+  # decompose
+  dc <- decompTSbfast(tsi, 6, 12)
+
+  #compare simulated and derived seasonality
+  diffseas <- mean(as.numeric(dc$Seasonality[,-c(1,2)]) - seas)
+
+  # test
+  expect_equal(diffseas, 0, tolerance = 1e-4)
 
 })
+
+
