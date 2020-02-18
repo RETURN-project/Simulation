3863c88f5f0c78355725b73336779c9dc694411a
wandadk <wandadekeersmaecker@gmail.com>
2020-02-18 08:16:33 +0100

    convert the rmarkdown (.Rmd) function files to .R files. Add some tests

--- tests/testthat/test-example.R
+++ tests/testthat/test-example.R
@@ -45,3 +45,60 @@ test_that("Decomposition harmonic + trend + noise", {
   expect_equal(diffseas, 0, tolerance = 1e-4)
 })
 
+
+test_that("ARMA coefficients - white noise",{
+  source_rmd <- function(file, local = FALSE, ...){
+    library('knitr')
+    options(knitr.duplicate.label = 'allow')
+
+    tempR <- tempfile(tmpdir = ".", fileext = ".R")
+    on.exit(unlink(tempR))
+    knitr::purl(file, output=tempR, quiet = TRUE)#
+
+    # source(tempR)
+    envir <- globalenv()
+    source(tempR, local = envir, ...)
+  }
+
+  source_rmd('fun_simulate.Rmd')
+  # Generate white noise
+  set.seed(197)
+  rn <- arima.sim(model = list(order = c(0, 0, 0) ), n = 2000)
+  ma1 <- arima.sim(model = list(order = c(0, 0, 1), ma = .9 ), n = 2000)
+  ar1 <- arima.sim(model = list(order = c(1, 0, 0), ar = .9 ), n = 2000)
+  ma2 <- arima.sim(model = list(order = c(0, 0, 2), ma = c(.7, .2) ), n = 2000)
+  getARMAcoef(rn)
+  getARMAcoef(ma1)
+  getARMAcoef(ar1)
+  getARMAcoef(ma2)
+
+})
+
+
+# ARMA coefficients
+getARMAcoef <- function(tsx){
+
+  tsx <- ma2
+
+
+  arm <- auto.arima(tsx, seasonal=F) #arm$arma #A compact form of the specification, as a vector giving
+  #the number of AR, MA, seasonal AR and seasonal MA coefficients,
+  #plus the period, and the number of non-seasonal, and seasonal differences.
+  teller <- 0
+  coefmod <- list()
+  # number of AR coef
+  if(arm$arma[1]>0){
+    coefmod <- c(coefmod, ar = as.numeric(arm$coef[1:arm$arma[1]]))
+    teller <- teller + arm$arma[1]
+  }
+  # number of MA coef
+  if(arm$arma[2]>0){
+    coefmod <- c(coefmod, ma = as.numeric(arm$coef[(1+teller):(teller+arm$arma[2])]))
+    #teller <- teller + arm$arma[2]
+  }
+  coefmod <- c(coefmod, order = as.numeric(c(arm$arma[1], arm$arma[6], arm$arma[2])))
+  coefmod
+}
+
+
+
