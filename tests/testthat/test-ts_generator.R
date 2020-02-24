1111b6a529709d80c8d20bd63bd7d3218c16283b
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2020-02-24 16:41:51 +0100

    fix typo in test

--- tests/testthat/test-ts_generator.R
+++ tests/testthat/test-ts_generator.R
@@ -4,18 +4,22 @@ test_that("Exponential decay", {
 
   ts <- seq(0, 25, by = 0.1)
   pert <- 1.5
-  ys <- exponential(ts, pert = pert)
+  tpert <- 0
+  thalf <- 1
 
-  expect_equal(min(ys), 0, tolerance = 1e-4)
-  expect_equal(max(ys), pert, tolerance = 1e-4)
+  ys <- exponential(ts, pert = pert, tpert = tpert, thalf = thalf)
+
+  expect_equal(abs(min(ys)), 0, tolerance = 1e-4)
+  expect_equal(abs(max(ys)), abs(pert), tolerance = 1e-4)
+
 })
 
 test_that("Linear decay", {
 
-  ts <- seq(0, 25, by = 0.1)
+  ts <- seq(0, 5, by = 0.01)
   pert <- 1.5
-  ys <- exponential(ts, pert = pert)
+  ys <- piecewise(ts, pert = pert)
 
-  expect_equal(min(ys), 0, tolerance = 1e-4)
-  expect_equal(max(ys), pert, tolerance = 1e-4)
+  expect_equal(abs(min(ys)), 0, tolerance = 1e-4)
+  expect_equal(abs(max(ys)), abs(pert), tolerance = 1e-4)
 })
