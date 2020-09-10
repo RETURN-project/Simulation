c046429bdd4850d6f09f81952e31ff8afd6631d3
wandadk <wandadekeersmaecker@gmail.com>
2020-09-10 09:46:23 +0200

    initial, very simple test for the evalParam function

--- tests/testthat/test-fun_recovery.R
+++ tests/testthat/test-fun_recovery.R
@@ -152,3 +152,69 @@ test_that("Frazier - segmented annual - short", {
   expect_equal(metrics$R80P, r80pm, tolerance = 1e-4)
   expect_equal(metrics$YrYr, yryrm, tolerance = 1e-4)
 })
+
+test_that("Evaluate recovery indicators - perfect fit", {
+  #-------------------------------------------------
+  #  settings  simulation
+
+  basename <- 'test'
+  # generate some time series characteristics - no noise, no missing values and small seasonal amplitude
+  set.seed(197)
+  STnobsYr <- 365# observations per year
+  Vqntl <- c(0.75,.95)# set of quantiles used to derive realistic values
+  seasVImax <- rep(0,100) #runif(100, 0.1, 0.2)# seasonal amplitude
+  tsVIMissVal <- rep(0,100)#runif(100,1/12,6/12)# missing values
+  Rem_VIsd <- rep(0,100)#runif(100,0.00001,0.00002)# sd remainder
+  TrVImean <- 0.7# offset
+  seasVImean <- 0.01* rep(sin(seq(0,pi*2,pi/183))[1:365],6)# seasonal pattern
+  Rem_VIcoef <- list(list(order1 =0, order2=0, order3=0))# ARMA coefficients remainder
+
+  # generate a settings list to simulate time series
+  sttngs <- list()
+  sttngs$'seasAmp' <- list(type = 'dist', vals = c(0,0,quantile(seasVImax, Vqntl)), obs = seasVImax, fix = quantile(seasVImax, c(.4, .6)))
+  sttngs$'missVal' <- list(type = 'dist',  vals = c(1-1/16,1-1/16,quantile(tsVIMissVal, Vqntl)), obs = tsVIMissVal, fix = quantile(tsVIMissVal, c(.4, .6)))
+  sttngs$'remSd' <- list(type = 'dist',  vals = c(0,0,quantile(Rem_VIsd, Vqntl)), obs = Rem_VIsd, fix = quantile(Rem_VIsd, c(.4, .6)))
+  sttngs$'nyr' <- list(type = 'range', vals = c(25), fix = 25)#seq(6,36, by = 6)
+  sttngs$'distMag' <- list(type = 'range', vals = -c(0.5, 0.5), fix = -c(0.25,0.35))
+  sttngs$'distT' <- list(type = 'range', vals = c(15,16), fix = c(15,16))#seq(3,33, by = 6)
+  sttngs$'distRec' <- list(type = 'range', vals = c(0.5,2,4,6.5)*STnobsYr, fix = c(2.25,3.75)*STnobsYr) #seq(0.5,6.5,by=0.5)
+  sttngs$'nDr' <- list(type = 'range',  vals = c(0), fix = 0)
+  sttngs$'distType' <- list(type = 'cat',  vals = c('piecewise'), fix = 'piecewise')#piecewise
+  sttngs$'DistMissVal' <- list(type = 'cat', vals = 'random', fix = 'random')
+  sttngs$'trAv' <- list(type = 'range', vals = TrVImean, fix = TrVImean)
+  sttngs$'general' <- list(
+    eval = c('distMag'),#parameters to be evaluated, can be  'distT','distRec', 'missVal' 'distMag', 'seasAmp', 'remSd'
+    nTS = 10,
+    nobsYr = STnobsYr,
+    seasAv = seasVImean,
+    remcoef = Rem_VIcoef,
+    parSetUp = 'int')#parameter set-up: can be avg dist, comb, or int
+
+  # settings of the recovery indicators
+  funSet <- list('freq' = 'dense',#rep('dense',3),
+                 'input' = 'raw',#c('raw', 'smoothed','segmented'),# settings for the recovery indicators
+                 'shortDenseTS' = T,# rep(TRUE,3),
+                 'nPre' = 2,# rep(2,3),
+                 'nDist' = 1,#rep(1,3),
+                 'nPostMin' = 4,#c(4,4,4),
+                 'nPostMax' = 6,#rep(6,3),
+                 'h' = 0.15,#rep(0.15,3),
+                 'seas' = T)#rep(T,3))
+  # evaluate recovery indicators
+  perf <- evalParam('distMag', sttngs, funSet, basename, ofolder = '')
+
+  # we expect a nearly perfect performance
+  expect_equal(length(perf), 12, tolerance = 1e-4)
+  expect_equal(perf$RRI_nTS$`-0.5`, 1, tolerance = 1e-4)
+  expect_equal(perf$R80p_nTS$`-0.5`, 1, tolerance = 1e-4)
+  expect_equal(perf$YrYr_nTS$`-0.5`, 1, tolerance = 1e-4)
+  expect_equal(perf$RRI_rsq$`-0.5`, 1, tolerance = 1e-4)
+  expect_equal(perf$R80p_rsq$`-0.5`, 1, tolerance = 1e-4)
+  expect_equal(perf$YrYr_rsq$`-0.5`, 1, tolerance = 1e-4)
+  expect_equal(perf$RRI_rmse$`-0.5`, 0, tolerance = 1e-4)
+  expect_equal(perf$R80p_rmse$`-0.5`, 0, tolerance = 1e-4)
+  expect_equal(perf$YrYr_rmse$`-0.5`, 0, tolerance = 1e-4)
+  expect_equal(perf$RRI_mape$`-0.5`, 0, tolerance = 1e-4)
+  expect_equal(perf$R80p_mape$`-0.5`, 0, tolerance = 1e-4)
+  expect_equal(perf$YrYr_mape$`-0.5`, 0, tolerance = 1e-4)
+})
